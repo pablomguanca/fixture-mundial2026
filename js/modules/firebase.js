e150902c7df272939c982bc414f04369a5c7722d
PabloGuanca <pablo.m.guanca@gmail.com>
2026-06-22 22:41:00 -0300

    chore: firebaseConfig via variables de entorno

--- js/modules/firebase.js
+++ js/modules/firebase.js
@@ -1,15 +1,7 @@
+import { firebaseConfig } from "../config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBe_Vha1klMazBHGEb2bpBRMxpILMmKXp8",
-  authDomain: "fixture-mundial-wc2026.firebaseapp.com",
-  projectId: "fixture-mundial-wc2026",
-  storageBucket: "fixture-mundial-wc2026.firebasestorage.app",
-  messagingSenderId: "334140254771",
-  appId: "1:334140254771:web:e7e27df57bf04919659146"
-};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
